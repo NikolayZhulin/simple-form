d65ed7da547100763a94f7690b3778e2c3ddecf5
NikolayZhulin <nikolayzhulin97@mail.ru>
2022-11-27 20:19:14 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,74 @@
-import React from 'react';
+import React, {ChangeEvent, FormEvent, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+type ValuesType = {
+    firstName: string,
+    lastName: string,
+    email: string,
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [values, setValues] = useState<ValuesType>({
+        firstName: '',
+        lastName: '',
+        email: '',
+    })
+    const [valid, setValid] = useState<boolean>(false)
+    const [submitted, setSubmitted] = useState<boolean>(false)
+
+    const onChangeFirstNameHandler = (e: ChangeEvent<HTMLInputElement>) => {
+            setValues({...values, firstName: e.currentTarget.value})
+    }
+    const onChangeLastNameHandler = (e: ChangeEvent<HTMLInputElement>) => {
+            setValues({...values, lastName: e.currentTarget.value})
+    }
+    const onChangeEmailHandler = (e: ChangeEvent<HTMLInputElement>) => {
+            setValues({...values, email: e.currentTarget.value})
+    }
+
+    const submitHandler = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        if (values.firstName && values.lastName && values.email) {
+            setValid(true)
+        }
+        setSubmitted(true)
+    }
+
+    return (
+        <div className="form-container">
+            <form className={'register-form'} onSubmit={submitHandler}>
+                {submitted && valid ? <div className={'success-message'}>Thank you for submitted!</div> : null}
+                <input
+                    onChange={onChangeFirstNameHandler}
+                    value={values.firstName}
+                    className={'form-field'}
+                    placeholder={'First name'}
+                    name={'firstName'}/>
+                {submitted && !values.firstName ? <span className={'error-message'}>Please enter a first name</span> : null}
+                <input
+                    onChange={onChangeLastNameHandler}
+                    value={values.lastName}
+                    className={'form-field'}
+                    placeholder={'Last name'}
+                    name={'lastName'}/>
+                {submitted && !values.lastName ? <span className={'error-message'}>Please enter a last name</span> : null}
+                <input
+                    onChange={onChangeEmailHandler}
+                    value={values.email}
+                    className={'form-field'}
+                    placeholder={'Email'}
+                    name={'Email'}/>
+                {submitted && !values.email ? <span className={'error-message'}>Please enter a email</span> : null}
+                <button
+                    className={'form-field'}
+                    type={"submit"}>Register
+                </button>
+            </form>
+
+        </div>
+    );
 }
 
 export default App;
