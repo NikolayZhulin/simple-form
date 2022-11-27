dd0ccddd407d55fac10c39f35ba3eae0655bc86e
NikolayZhulin <nikolayzhulin97@mail.ru>
2022-11-27 20:23:37 +0100

    change styles

--- src/App.tsx
+++ src/App.tsx
@@ -40,27 +40,33 @@ function App() {
         <div className="form-container">
             <form className={'register-form'} onSubmit={submitHandler}>
                 {submitted && valid ? <div className={'success-message'}>Thank you for submitted!</div> : null}
-                <input
-                    onChange={onChangeFirstNameHandler}
-                    value={values.firstName}
-                    className={'form-field'}
-                    placeholder={'First name'}
-                    name={'firstName'}/>
-                {submitted && !values.firstName ? <span className={'error-message'}>Please enter a first name</span> : null}
-                <input
-                    onChange={onChangeLastNameHandler}
-                    value={values.lastName}
-                    className={'form-field'}
-                    placeholder={'Last name'}
-                    name={'lastName'}/>
-                {submitted && !values.lastName ? <span className={'error-message'}>Please enter a last name</span> : null}
-                <input
-                    onChange={onChangeEmailHandler}
-                    value={values.email}
-                    className={'form-field'}
-                    placeholder={'Email'}
-                    name={'Email'}/>
-                {submitted && !values.email ? <span className={'error-message'}>Please enter a email</span> : null}
+                <div>
+                    <input
+                        onChange={onChangeFirstNameHandler}
+                        value={values.firstName}
+                        className={'form-field'}
+                        placeholder={'First name'}
+                        name={'firstName'}/>
+                    {submitted && !values.firstName ? <span className={'error-message'}>Please enter a first name</span> : null}
+                </div>
+                <div>
+                    <input
+                        onChange={onChangeLastNameHandler}
+                        value={values.lastName}
+                        className={'form-field'}
+                        placeholder={'Last name'}
+                        name={'lastName'}/>
+                    {submitted && !values.lastName ? <span className={'error-message'}>Please enter a last name</span> : null}
+                </div>
+                <div>
+                    <input
+                        onChange={onChangeEmailHandler}
+                        value={values.email}
+                        className={'form-field'}
+                        placeholder={'Email'}
+                        name={'Email'}/>
+                    {submitted && !values.email ? <span className={'error-message'}>Please enter a email</span> : null}
+                </div>
                 <button
                     className={'form-field'}
                     type={"submit"}>Register
